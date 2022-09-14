9a3506262df0ccde440481c5799dd61e38cad9f0
Romain Jouhannet <romain.jouhannet@packetfabric.com>
2022-09-14 11:57:33 -0400

    add terraform cloud demo

--- terraform_cloud_demo/main.tf
+++ terraform_cloud_demo/main.tf
@@ -12,13 +12,10 @@ provider "packetfabric" {
   token = var.pf_api_key
 }
 
-# Create random name to use to name objects
-resource "random_pet" "name" {}
-
 resource "packetfabric_cloud_router" "cr" {
   provider     = packetfabric
   asn          = var.pf_cr_asn
-  name         = "${var.tag_name}-${random_pet.name.id}"
+  name         = var.tag_name
   account_uuid = var.pf_account_uuid
   capacity     = var.pf_cr_capacity
   regions      = var.pf_cr_regions
@@ -26,7 +23,7 @@ resource "packetfabric_cloud_router" "cr" {
 
 resource "packetfabric_aws_cloud_router_connection" "crc_1" {
   provider       = packetfabric
-  description    = "${var.tag_name}-${random_pet.name.id}-${var.pf_crc_pop1}"
+  description    = "${var.tag_name}-${var.pf_crc_pop1}"
   circuit_id     = packetfabric_cloud_router.cr.id
   account_uuid   = var.pf_account_uuid
   aws_account_id = var.pf_aws_account_id
