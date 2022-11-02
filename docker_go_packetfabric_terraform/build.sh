7c7ac2a8360926589f6fd2d944c67886f71ba05c
Romain Jouhannet <romain.jouhannet@packetfabric.com>
2022-11-02 09:43:13 -0700

    add aws local provider

--- docker_go_packetfabric_terraform/build.sh
+++ docker_go_packetfabric_terraform/build.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-cd terraform-provider-packetfabric
+# do the gut pull on the branch is at and removing old branches
+cd resources/terraform-provider-packetfabric
 git fetch
 git status
 git branch | grep -v $(git rev-parse --abbrev-ref HEAD) | xargs git branch -D
 git pull
 echo
 cd -
+# remove old docker image
 docker rmi terraform-runner
+# build the new one
 docker build -t terraform-runner .
+# list the new image
 docker images | grep terr
